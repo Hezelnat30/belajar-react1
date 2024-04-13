9e09f7a0805827df76e37b0b7e5fef3c21e67972
Hezelnat30 <justin.hezekiel123@gmail.com>
2024-04-13 11:37:01 +0700

    update method

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -4,21 +4,31 @@ import { useState } from "react";
 function InputSearch(props) {
   const [search, setSearch] = useState("");
 
-  const changeSearch = (e) => {
-    setSearch(e.target.value);
-    props.onSearch(e.target.value);
+  const handleSearch = () => props.onSearch(search);
+
+  const searchKeyDown = (e) => {
+    e.key === "Enter" ? handleSearch() : "";
   };
 
   return (
     <div className="p-2 flex max-w-md mx-auto flex-wrap gap-3 items-center justify-center">
-      <label htmlFor="search" className="text-white text-lg">
-        Cari Artikel :
-      </label>
-      <input
-        onChange={changeSearch}
-        type="text"
-        className="px-2 py-1 rounded bg-slate-100 outline-none focus:ring-2 focus:ring-indigo-600 focus:border-indigo-600"
-      />
+      <div className="form-search flex">
+        <label htmlFor="search" className="text-white me-2 text-lg">
+          Cari Artikel :
+        </label>
+        <input
+          onKeyDown={searchKeyDown}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          className="px-2 py-1 rounded-l bg-slate-100 outline-none focus:ring-2 focus:ring-indigo-600 focus:border-indigo-600"
+        />
+        <button
+          onClick={handleSearch}
+          className="px-2 py-1 rounded-r transition-all duration-100 bg-indigo-500 text-white hover:bg-indigo-400"
+        >
+          Cari
+        </button>
+      </div>
       <small className="text-white text-base">
         Ditemukan {props.totalPosts} data dengan kata pencarian {search}
       </small>
